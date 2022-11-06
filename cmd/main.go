25d4f2d812adbc4944fd2a5904ae87b88a1bc076
Pedro Leal <git@pedroleal.pt>
2022-11-06 00:02:50 +0000

    Random map for storing components

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,25 @@ const (
 	PORT = "7170"
 )
 
-func main() {
-	server := internal.NewServer(HOST, PORT)
-	storageManager := internal.NewStorageManager(server)
-	internal.NewSessionManager(server)
-	internal.NewDispatcher(server, storageManager)
+const (
+	Server         int = 0
+	StorageManager     = 1
+	SessionManager     = 2
+	Dispatcher         = 3
+)
 
-	server.Start()
+func main() {
+	m := make(map[int]any)
+
+	serverInstance := internal.NewServer(HOST, PORT)
+	storageManager := internal.NewStorageManager(serverInstance)
+	sessionManager := internal.NewSessionManager(serverInstance)
+	dispatcher := internal.NewDispatcher(serverInstance, storageManager)
+
+	m[Server] = serverInstance
+	m[StorageManager] = storageManager
+	m[SessionManager] = sessionManager
+	m[Dispatcher] = dispatcher
+
+	serverInstance.Start()
 }
