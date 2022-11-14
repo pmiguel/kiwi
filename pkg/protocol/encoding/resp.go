f780622fa59d2feb08e06968dea3f0928e0967c7
Pedro Leal <git@pedroleal.pt>
2022-11-14 23:58:27 +0000

    Unit tests for decoders and encoders for RESP

--- pkg/protocol/encoding/resp.go
+++ pkg/protocol/encoding/resp.go
@@ -8,9 +8,9 @@ type Int int64
 type SimpleString string
 type BulkString string
 type Error string
-type Array []RestType
+type Array []Resp
 
-type RestType interface {
+type Resp interface {
 	Encode() string
 }
 
@@ -23,7 +23,7 @@ const (
 	separator          = "\r\n"
 )
 
-func (i *Int) Encode() string {
+func (i Int) Encode() string {
 	return fmt.Sprintf("%c%d%s", prefixInteger, i, separator)
 }
 
@@ -31,13 +31,13 @@ func DecodeInt(input string) Int {
 	var out Int
 	_, err := fmt.Sscanf(input, ":%d\r\n", &out)
 	if err != nil {
-		return 0
+		return Int(0)
 	}
-	return out
+	return Int(out)
 }
 
-func (str *SimpleString) Encode() string {
-	return fmt.Sprintf("%c%s%s", prefixSimpleString, *str, separator)
+func (str SimpleString) Encode() string {
+	return fmt.Sprintf("%c%s%s", prefixSimpleString, str, separator)
 }
 
 func DecodeSimpleString(input string) SimpleString {
@@ -49,8 +49,8 @@ func DecodeSimpleString(input string) SimpleString {
 	return out
 }
 
-func (str *BulkString) Encode() string {
-	return fmt.Sprintf("%c%d%s%s%s", prefixBulkString, len(*str), separator, *str, separator)
+func (str BulkString) Encode() string {
+	return fmt.Sprintf("%c%d%s%s%s", prefixBulkString, len(str), separator, str, separator)
 }
 
 func DecodeBulkString(input string) BulkString {
@@ -63,27 +63,30 @@ func DecodeBulkString(input string) BulkString {
 	return out
 }
 
-func (str *Error) Encode() string {
-	return fmt.Sprintf("%c%s%s", prefixError, *str, separator)
+func (str Error) Encode() string {
+	return fmt.Sprintf("%c%s%s", prefixError, str, separator)
 }
 
 func DecodeError(input string) Error {
 	var out Error
-	_, err := fmt.Sscanf(input, "+%s\r\n", &out)
+	_, err := fmt.Sscanf(input, "-%s\r\n", &out)
 	if err != nil {
 		return ""
 	}
 	return out
 }
 
-func (arr Array) Encode() string {
+func (arr *Array) Encode() string {
 	if arr == nil {
 		return fmt.Sprintf("%c%d%s", prefixArray, -1, separator)
 	}
-	itemCount := len(arr)
+
+	resolved := *arr
+
+	itemCount := len(resolved)
 	buff := fmt.Sprintf("%c%d%s", prefixArray, itemCount, separator)
 	for i := 0; i < itemCount; i++ {
-		value := arr[i].Encode()
+		value := resolved[i].Encode()
 		buff += fmt.Sprintf("%s", value)
 	}
 	return buff
